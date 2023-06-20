1a107abdf67a219dce6cf9b7d65df691ab97a7d7
theoroton <rotont@NCY-LAP-DEV-05.mmc.lo>
2023-06-20 16:22:39 +0200

    Cours P2.3 : Création de modèles pour la base de données

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Création model/classe groupe
+class Band(models.Model):
+    name = models.fields.CharField(max_length=100) # Attibut name : Character Field -> champ qui stocke une chaine d'une longueur max de 100
+
+
+# Création modèle Affiche
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100) # Attribut title
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse # Import nécessaire
 
+from listings.models import Band # Modèle Band importé
+from listings.models import Listing # Modèle Listing importé
+
+
 # Fonction pour une première page
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+
+    # Réponse HTTP à afficher retourné
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Groupes :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
+
 
 
 # Fonction pour la page à propos
@@ -11,11 +27,25 @@ def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous adorons merch !</p>')
 
 
+
 # Fonction pour le formulaire de contact
 def contact(request):
     return HttpResponse('<h1>Formulaire de contact</h1>')
 
 
+
 # Fonction pour la page de listing
 def listings(request):
-    return HttpResponse('<h1>Listings</h1>')
+    listings = Listing.objects.all()
+
+    response = """<h1>Listings</h1>
+                  <ul>
+                """
+
+    for listing in listings:
+        response += f"<li>{listing.title}</li>"
+
+
+    response += "<ul>"
+
+    return HttpResponse(response)
